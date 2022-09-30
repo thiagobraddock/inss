ce8acfc061892c9de2b8d86c5e2132934ad290ba
thiago braddock <braddock01@hotmail.com>
2022-09-30 15:20:59 -0300

    fix: endpoint object

--- pages/api/course.js
+++ pages/api/course.js
@@ -2,8 +2,8 @@ function course(request, response) {
   const course = [
     {
       id: 'fundamentals',
-      modulo: 'Fundamentos',
-      descricao:
+      module: 'Fundamentos',
+      description:
         'Nosso primeiro módulo te ensinará tudo que você precisa saber para iniciar sua carreira no mundo do desenvolvimento Web, virando uma pessoa preparada para os módulos de front-end e back-end. Vamos te guiar, etapa a etapa, por toda essa jornada!',
       color: '#BDD5FF',
       section: [
@@ -15,8 +15,8 @@ function course(request, response) {
     },
     {
       id: 'front',
-      modulo: 'Front-end',
-      descricao:
+      module: 'Front-end',
+      description:
         'Neste módulo, você vai aprender as técnicas e ferramentas mais atuais para desenvolver um código de front-end que seja rápido, bonito e testável.',
       color: '#A0F046',
       section: [
@@ -28,8 +28,8 @@ function course(request, response) {
     },
     {
       id: 'back',
-      modulo: 'Back-end',
-      descricao:
+      module: 'Back-end',
+      description:
         'Neste módulo, você vai aprender como desenvolver um código de back-end robusto, limpo, escalável e seguro, dominando o uso de bancos de dados, a construção de APIs com testes automatizados.',
       color: '#FCF1CF',
       section: [
@@ -41,8 +41,8 @@ function course(request, response) {
     },
     {
       id: 'css',
-      modulo: 'Ciência da Computação',
-      descricao:
+      module: 'Ciência da Computação',
+      description:
         'Aqui você aprenderá os principais conceitos da Ciência da Computação que são aplicados no dia a dia do desenvolvimento de software e como transformar a teoria em prática, analisando algoritmos e escolhendo as melhores estruturas de dados para resolver os problemas!',
       color: '#F3C5F3',
       section: [
