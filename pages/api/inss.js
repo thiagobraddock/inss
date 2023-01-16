0e8534ccb97acf4b26ba9f16b114a51d99fe3d0c
Thiago "BRADDOCK" Oliveira <braddock01@hotmail.com>
2023-01-16 14:26:33 -0300

    feat: atualiza tabela contribuicao 2023

--- pages/api/inss.js
+++ pages/api/inss.js
@@ -1,10 +1,11 @@
 function inss(request, response) {
   const contribuicao = [
-    { faixa: 1, contrInicio: 0, contrFim: 1212.0, aliq: 0.075, agreg: 0 },
-    { faixa: 2, contrInicio: 1212.01, contrFim: 2427.35, aliq: 0.09, agreg: 0 },
-    { faixa: 3, contrInicio: 2427.36, contrFim: 3641.03, aliq: 0.12, agreg: 0 },
-    { faixa: 4, contrInicio: 3641.04, contrFim: 7087.22, aliq: 0.14, agreg: 0 },
+    { faixa: 1, contrInicio: 0, contrFim: 1302.0, aliq: 0.075, agreg: 0 },
+    { faixa: 2, contrInicio: 1302.01, contrFim: 2571.29, aliq: 0.09, agreg: 0 },
+    { faixa: 3, contrInicio: 2571.3, contrFim: 3856.94, aliq: 0.12, agreg: 0 },
+    { faixa: 4, contrInicio: 3856.95, contrFim: 7507.49, aliq: 0.14, agreg: 0 },
   ];
+
   let acumulado = 0;
   contribuicao.forEach((faixa) => {
     faixa.agreg = acumulado;
@@ -12,7 +13,7 @@ function inss(request, response) {
   });
 
   response.status(200).json({
-    ano: 2022,
+    ano: 2023,
     contribuicao,
   });
 }
